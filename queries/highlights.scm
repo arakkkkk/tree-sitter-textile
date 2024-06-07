40b02ffb62b4d597d13d81d2a9086d4d05fd25d2
arakkk <bdkouiti5013@gmail.com>
2024-06-07 10:58:21 +0900

    update

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,5 @@
 ;From nvim-treesitter/nvim-treesitter
-(atx_heading (inline) @markup.heading)
+; (atx_heading (inline) @markup.heading)
 
 [
   (atx_h1_marker)
@@ -17,3 +17,27 @@
 [
   (list_marker)
 ] @punctuation.special
+
+(atx_heading
+  (atx_h1_marker) @markup.heading.1
+  (inline) @markup.heading.1)
+
+(atx_heading
+  (atx_h2_marker) @markup.heading.2
+  (inline) @markup.heading.2)
+
+(atx_heading
+  (atx_h3_marker) @markup.heading.3
+  (inline) @markup.heading.3)
+
+(atx_heading
+  (atx_h4_marker) @markup.heading.4
+  (inline) @markup.heading.4)
+
+(atx_heading
+  (atx_h5_marker) @markup.heading.5
+  (inline) @markup.heading.5)
+
+(atx_heading
+  (atx_h6_marker) @markup.heading.6
+  (inline) @markup.heading.6)
